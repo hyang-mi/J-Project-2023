d128eda793f5e41c3b4e1087d0c91df647218135
hongkikii <hyangmeeh@icloud.com>
2023-07-03 10:39:55 +0900

    서버 프로젝트 파일 설정

---
+++ JProject/src/test/java/com/projectJ/JProject/JProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectJ.JProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
