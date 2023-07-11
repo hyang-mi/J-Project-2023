4cb32f22ee4f4cad1a0826f704f4cea3c9f79248
hyang-mi <hyangmeeh@icloud.com>
2023-07-11 13:04:28 +0900

    .gitignore 업데이트

--- JProject/src/test/java/com/mjuAppSW/appName/domain/member/ListTest.java
+++
@@ -1,9 +0,0 @@
-package com.mjuAppSW.appName.domain.member;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class ListTest {
-
-    // 다른 사람 상세 프로필 요청
-}
--- JProject/src/test/java/com/mjuAppSW/appName/domain/member/LoginTest.java
+++
@@ -1,7 +0,0 @@
-package com.mjuAppSW.appName.domain.member;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class LoginTest {
-}
