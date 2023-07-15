02f3a8ed9e34404edd0b31b233bbf39644dfb8fb
hyang-mi <hyangmeeh@icloud.com>
2023-07-15 23:17:47 +0900

    .gitignore 수정

--- JProject/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Member (member_id, name, k_id, basic_profile) VALUES (1, 'memberA', 1, true);
-INSERT INTO Member (member_id, name, k_id, basic_profile) VALUES (2, 'memberB', 2, true);
-INSERT INTO Member (member_id, name, k_id, basic_profile) VALUES (3, 'memberC', 3, true);
-INSERT INTO Member (member_id, name, k_id, basic_profile) VALUES (4, 'memberD', 4, true);
